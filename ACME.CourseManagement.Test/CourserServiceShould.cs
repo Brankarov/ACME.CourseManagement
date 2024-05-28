a66b097f06cd4a658eebb5f95d6ca390b125e963
Leonel Sebastian Gergoff <leo_seba_93@hotmail.com>
2024-05-28 18:44:47 -0300

    Added Coursepayment Test

---
+++ ACME.CourseManagement.Test/CourserServiceShould.cs
@@ -0,0 +1,41 @@
+﻿using ACME.CourseManagement.Test.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACME.CourseManagement.Test
+{
+    public class CourserServiceShould : ServicesTest
+    {
+        [Fact]
+        public void PaymentSuccessful()
+        {
+            var request = AddPaymentRequest.BigMoney;
+            var result = courseService.PaymentCourse(request);
+            Assert.True(result);
+        }
+        [Fact]
+        public void PaymentFailed() 
+        {
+            var request = AddPaymentRequest.LowMoney;
+            var result = courseService.PaymentCourse(request);
+            Assert.False(result);
+        }
+        [Fact]
+        public void PaymentWithWrongCourse()
+        {
+            var request = AddPaymentRequest.wrongCourse;
+            courseService.PaymentCourse(request);
+            Assert.Throws<Exception>(() => { courseService.PaymentCourse(request); });
+        }
+        [Fact]
+        public void PaymentWithnegativeMoney() 
+        {
+            var request = AddPaymentRequest.Debts;
+            courseService.PaymentCourse(request);
+            Assert.Throws<Exception>(() => { courseService.PaymentCourse(request); }) ;
+        }
+    }
+}
